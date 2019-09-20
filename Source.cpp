c934c7cabfa293bd18199ebc067a0ce6c13ca440
Karthik Iyer <Karthik.iyer2@yandex.com>
2019-09-21 01:50:49 +0530

    add antialiasing

--- Source.cpp
+++ Source.cpp
@@ -4,24 +4,8 @@
 #include "ray.h"
 #include "hitable_list.h"
 #include "sphere.h"
+#include "camera.h"
 using namespace std;
-
-//float hit_sphere(const vec3& center, float radius, const ray& r) {
-//
-//	vec3 oc = r.origin() - center;
-//	float a = dot(r.direction(), r.direction());
-//	float b = 2.0f * dot(oc, r.direction());
-//	float c = dot(oc, oc) - radius * radius;
-//	float discriminant = b * b - 4 * a * c;
-//	if (discriminant < 0)
-//	{
-//		return -1.0f;
-//	}
-//	else {
-//		return (-b - sqrt(discriminant)) / (2.0f * a);
-//	}
-//
-//}
 
 vec3 color(const ray& r, hitable* world) {
 	hit_record rec;
@@ -40,11 +24,7 @@ int main() {
 	fout.open("img.ppm");
 	int nx = 400;
 	int ny = 200;
-
-	vec3 lower_left_corner(-2.0f, -1.0f, -1.0f);
-	vec3 horizontal(4.0f, 0.0f, 0.0f);
-	vec3 vertical(0.0f, 2.0f, 0.0f);
-	vec3 origin(0.0f, 0.0f, 0.0f);
+	int ns = 100;
 
 	hitable* list[2];
 	list[0] = new sphere(vec3(0, 0, -1), 0.5);
@@ -52,19 +32,25 @@ int main() {
 
 	hitable* world = new hitable_list(list, 2);
 
+	camera cam;
+
 	if (fout) {
 	
 		fout << "P3\n" << nx << " " << ny << "\n255\n";
 		for (int j = ny - 1; j >= 0; j--) {
 		
 			for (int i = 0; i < nx; i++) {
-			
-				float u = float(i) / float(nx);
-				float v = float(j) / float(ny);
-				ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+				vec3 col(0, 0, 0);
+				for (int s = 0; s < ns; s++) {
+					float u = (float(i) + (float(rand()) / float(RAND_MAX))) / float(nx);
+					float v = (float(j) + (float(rand()) / float(RAND_MAX))) / float(ny);
 
-				vec3 p = r.point_at_parameter(2.0);
-				vec3 col = color(r, world);
+					ray r = cam.get_ray(u, v);
+					vec3 p = r.point_at_parameter(2.0);
+					col += color(r, world);
+				}
+				col /= float(ns);
+
 				int ir = int(255.99 * col[0]);
 				int ig = int(255.99 * col[1]);
 				int ib = int(255.99 * col[2]);
