b48a6bedfe92fdb336f8066b46c6d538b8581912
Karthik Iyer <Karthik.iyer2@yandex.com>
2019-10-06 01:47:43 +0530

    write PNG instead of PPM

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,5 @@
 #define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 
 #include<iostream>
 #include<fstream>
@@ -14,7 +15,9 @@
 #include "camera.h"
 #include "material.h"
 #include "texture.h"
+#include "bvh_node.h"
 #include "stb_image.h"
+#include "stb_image_write.h"
 
 using namespace std;
 
@@ -52,22 +55,22 @@ vec3 color(const ray& r, hitable* world, int depth) {
 hitable* random_scene() {
 	SKY = GRADIENT_SKY;
 	int n = 500;
-	hitable** list = new hitable * [n+1];
+	hitable** list = new hitable * [n + 1];
 
 	texture* checker = new checker_texture(new constant_texture(vec3(0.2, 0.3, 0.1)), new constant_texture(vec3(0.9, 0.9, 0.9)));
 
-	list[0] = new sphere(vec3(0,-1000,0), 1000, new lambertian(checker));
+	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(checker));
 
 	int i = 1;
 	for (int a = -11; a < 11; a++) {
 		for (int b = -11; b < 11; b++)
 		{
 			float choose_mat = random_number();
-			vec3 center(a+0.9*random_number(), 0.2, b+0.9*random_number());
+			vec3 center(a + 0.9 * random_number(), 0.2, b + 0.9 * random_number());
 			if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
 				if (choose_mat < 0.8)
 				{
-					list[i++] = new moving_sphere(center, center+vec3(0,0.5*random_number(),0), 0.0, 1.0, 0.2, new lambertian(new constant_texture(vec3(random_number() * random_number(), random_number() * random_number(), random_number() * random_number()))));
+					list[i++] = new moving_sphere(center, center + vec3(0, 0.5 * random_number(), 0), 0.0, 1.0, 0.2, new lambertian(new constant_texture(vec3(random_number() * random_number(), random_number() * random_number(), random_number() * random_number()))));
 				}
 				else if (choose_mat < 0.95)
 				{
@@ -82,9 +85,9 @@ hitable* random_scene() {
 	}
 	int nx, ny, nn;
 	unsigned char* tex_data = stbi_load("earth-map.jpg", &nx, &ny, &nn, 0);
-	//std::cout << nx << " " << ny << " " << nn << "\n";
-	material* mat = new lambertian(new image_texture(tex_data, nx, ny));
 	
+	material* mat = new lambertian(new image_texture(tex_data, nx, ny));
+
 	list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
 	list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(new constant_texture(vec3(0.4, 0.2, 0.1))));
 	list[i++] = new sphere(vec3(4, 1, 0), 1.0, mat);
@@ -92,6 +95,25 @@ hitable* random_scene() {
 
 	return new hitable_list(list, i);
 
+}
+
+hitable* simple_light() {
+	SKY = GRADIENT_SKY;
+	hitable** list = new hitable * [4];
+	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(new constant_texture(vec3(0.4, 0.2, 0.1))));
+	list[1] = new sphere(vec3(0, 2, 0), 2, new lambertian(new constant_texture(vec3(0.4, 0.2, 0.1))));
+	list[2] = new sphere(vec3(0, 7, 0), 2, new diffuse_light(new constant_texture(vec3(4, 4, 4))));
+	list[3] = new xy_rect(3, 5, 1, 3, -2, new diffuse_light(new constant_texture(vec3(4, 4, 4))));
+	return new hitable_list(list, 4);
+}
+
+hitable* two_perlin_spheres() {
+	SKY = GRADIENT_SKY;
+	texture* pertext = new noise_texture(4);
+	hitable** list = new hitable * [2];
+	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(pertext));
+	list[1] = new sphere(vec3(0, 2, 0), 2, new lambertian(pertext));
+	return new hitable_list(list, 2);
 }
 
 hitable* cornell_box() {
@@ -134,23 +156,54 @@ hitable* cornell_smoke() {
 	return new hitable_list(list, i);
 }
 
-hitable* simple_light() {
-	SKY = GRADIENT_SKY;
-	hitable** list = new hitable * [4];
-	list[0] = new sphere(vec3(0,-1000,0), 1000, new lambertian(new constant_texture(vec3(0.4, 0.2, 0.1))));
-	list[1] = new sphere(vec3(0, 2, 0), 2, new lambertian(new constant_texture(vec3(0.4, 0.2, 0.1))));
-	list[2] = new sphere(vec3(0, 7, 0), 2, new diffuse_light(new constant_texture(vec3(4, 4, 4))));
-	list[3] = new xy_rect(3, 5, 1, 3, -2, new diffuse_light(new constant_texture(vec3(4, 4, 4))));
-	return new hitable_list(list, 4);
-}
+hitable* final_scene() {
+	int nb = 20;
+	hitable** list = new hitable * [30];
+	hitable** box_list = new hitable * [10000];
+	hitable** box_list2 = new hitable * [10000];
 
-hitable* two_perlin_spheres() {
-	SKY = GRADIENT_SKY;
-	texture* pertext = new noise_texture(4);
-	hitable** list = new hitable * [2];
-	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(pertext));
-	list[1] = new sphere(vec3(0, 2, 0), 2, new lambertian(pertext));
-	return new hitable_list(list, 2);
+	material* white = new lambertian(new constant_texture(vec3(0.73, 0.73, 0.73)));
+	material* ground = new lambertian(new constant_texture(vec3(0.48, 0.83, 0.53)));
+	int b = 0;
+	for (int i = 0; i < nb; i++) {
+		for (int j = 0; j < nb; j++) {
+			float w = 100;
+			float x0 = -1000 + i * w;
+			float z0 = -1000 + j * w;
+			float y0 = 0;
+			float x1 = x0 + w;
+			float y1 = 100 * (random_number() + 0.01);
+			float z1 = z0 + w;
+			box_list[b++] = new box(vec3(x0, y0, z0), vec3(x1, y1, z1), ground);
+		}
+	}
+	int l = 0;
+	list[l++] = new bvh_node(box_list, b, 0, 1);
+	material* light = new diffuse_light(new constant_texture(vec3(7, 7, 7)));
+	list[l++] = new xz_rect(123, 423, 147, 412, 554, light);
+	vec3 center(400, 400, 200);
+	list[l++] = new moving_sphere(center, center + vec3(30, 0, 0), 0, 1, 50, new lambertian(new constant_texture(vec3(0.7, 0.3, 0.1))));
+	list[l++] = new sphere(vec3(260, 150, 45), 50, new dielectric(1.5));
+	list[l++] = new sphere(vec3(0, 150, 45), 50, new metal(vec3(0.8, 0.8, 0.9), 10.0));
+
+	hitable* boundary = new sphere(vec3(360, 150, 145), 70, new dielectric(1.5));
+	list[l++] = boundary;
+	list[l++] = new constant_medium(boundary, 0.2, new constant_texture(vec3(0.2, 0.4, 0.9)));
+
+	boundary = new sphere(vec3(0, 0, 0), 5000, new dielectric(1.5));
+	list[l++] = new constant_medium(boundary, 0.0001, new constant_texture(vec3(1.0, 1.0, 1.0)));
+	int nx, ny, nn;
+	unsigned char* tex_data = stbi_load("earth-map.jpg", &nx, &ny, &nn, 0);
+	material* emat = new lambertian(new image_texture(tex_data, nx, ny));
+	list[l++] = new sphere(vec3(400, 200, 400), 100, emat);
+	texture* pertext = new noise_texture(0.1);
+	list[l++] = new sphere(vec3(220, 280, 300), 80, new lambertian(pertext));
+	int ns = 1000;
+	for (int j = 0; j < ns; j++) {
+		box_list2[j] = new sphere(vec3(165 * random_number(), 165 * random_number(), 165 * random_number()), 10, white);
+	}
+	list[l++] = new translate(new rotate_y(new bvh_node(box_list2, ns, 0.0, 1.0), 15), vec3(-100, 270, 395));
+	return new hitable_list(list, l);
 }
 
 int clamp(int a, int min, int max) {
@@ -158,14 +211,14 @@ int clamp(int a, int min, int max) {
 }
 
 int main() {
-	ofstream fout;
-	fout.open("img.ppm");
-	int nx = 1080/5;
-	int ny = 1080/5;
-	int ns = 128*2;
+	/*ofstream fout;
+	fout.open("img.ppm");*/
+	int nx = 1080 / 5;
+	int ny = 1080 / 5;
+	int ns = 128;
 
 	/*hitable* list[5];
-	
+
 	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
 	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
 	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.0));
@@ -173,7 +226,7 @@ int main() {
 	list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));*/
 
 	//hitable* world = new hitable_list(list, 5);
-	
+
 	//scene setup for simple light
 	/*hitable* world = random_scene();
 	vec3 lookfrom(13, 2, 3);
@@ -210,6 +263,15 @@ int main() {
 	float vfov = 40.0;
 	camera cam(lookfrom, lookat, vec3(0, 1, 0), vfov, float(nx) / float(ny), aperture, dist_to_focus, 0.0, 1.0);
 
+	//scene setup for final scene
+	/*hitable* world = final_scene();
+	vec3 lookfrom(278, 278, -800);
+	vec3 lookat(278, 278, 0);
+	float dist_to_focus = 10.0;
+	float aperture = 0.0;
+	float vfov = 40.0;
+	camera cam(lookfrom, lookat, vec3(0, 1, 0), vfov, float(nx) / float(ny), aperture, dist_to_focus, 0.0, 1.0);*/
+
 	//scene setup for perlin sphere
 	/*hitable* world = two_perlin_spheres();
 	vec3 lookfrom(13, 2, 3);
@@ -219,33 +281,34 @@ int main() {
 	float vfov = 20.0;
 	camera cam(lookfrom, lookat, vec3(0, 1, 0), vfov, float(nx) / float(ny), aperture, dist_to_focus, 0.0, 1.0);*/
 
-	if (fout) {
-	
-		fout << "P3\n" << nx << " " << ny << "\n255\n";
-		for (int j = ny - 1; j >= 0; j--) {
-		
-			for (int i = 0; i < nx; i++) {
-				vec3 col(0, 0, 0);
-				for (int s = 0; s < ns; s++) {
-					float u = (float(i) + random_number()) / float(nx);
-					float v = (float(j) + random_number()) / float(ny);
+	unsigned char* buffer = new unsigned char[nx * ny * 3];
 
-					ray r = cam.get_ray(u, v);
-					vec3 p = r.point_at_parameter(2.0);
-					col += color(r, world, 0);
-				}
-				col /= float(ns);
-				col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
+	for (int j = ny - 1; j >= 0; j--) {
 
-				int ir = int(255.99 * col[0]);
-				int ig = int(255.99 * col[1]);
-				int ib = int(255.99 * col[2]);
-				fout << clamp(ir, 0, 255) << " " << clamp(ig, 0, 255) << " " << clamp(ib, 0, 255) << "\n";
+		for (int i = 0; i < nx; i++) {
+			vec3 col(0, 0, 0);
+			for (int s = 0; s < ns; s++) {
+				float u = (float(i) + random_number()) / float(nx);
+				float v = (float(j) + random_number()) / float(ny);
 
+				ray r = cam.get_ray(u, v);
+				vec3 p = r.point_at_parameter(2.0);
+				col += color(r, world, 0);
 			}
+			col /= float(ns);
+			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 
+			int ir = int(255.99 * col[0]);
+			int ig = int(255.99 * col[1]);
+			int ib = int(255.99 * col[2]);
+
+			buffer[3 * i + 3 * nx * (ny - 1 - j)] = clamp(ir, 0, 255);
+			buffer[3 * i + 3 * nx * (ny - 1 - j) + 1] = clamp(ig, 0, 255);
+			buffer[3 * i + 3 * nx * (ny - 1 - j) + 2] = clamp(ib, 0, 255);
 		}
 
 	}
+
+	stbi_write_png("img.png", nx, ny, 3, buffer, nx * 3);
 
 }
